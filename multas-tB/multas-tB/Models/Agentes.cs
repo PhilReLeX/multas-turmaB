141d67c4ab61caa30c76969de3e215011cfb9003
Filipe Dias <billabong.1998@hotmail.com>
2018-04-03 13:02:19 +0100

    adição de restrições ao modelo-Agentes + criações do interface (controller) sobre os Agentes

--- multas-tB/multas-tB/Models/Agentes.cs
+++ multas-tB/multas-tB/Models/Agentes.cs
@@ -16,8 +16,12 @@ namespace multas_tB.Models
         [Key]
         public int ID { get; set; } //chave primária
 
+        [Required(ErrorMessage ="O {0} é de preenchimento obrigatório!!")]
+        [RegularExpression("[A-ZÍÉÂÁ][a-záéíóúàèìòùâêîôûäëïöüçãõ]+(( |'|-| dos | da | de | e | d')[A-ZÍÉÂÁ][a-záéíóúàèìòùâêîôûäëïöüçãõ]+){1,3}", ErrorMessage ="O {0} apenas pode conter letras e espaços em branco. Cada palavra começa em Maiúscula, seguida de minúsculas.")]
         public string Nome { get; set; }
 
+        [Required(ErrorMessage = "A {0} é de preenchimento obrigatório!!")]
+        [RegularExpression("[A-ZÍÉÂÁ]*[a-záéíóúàèìòùâêîôûäëïöüçãõ -]*", ErrorMessage = "A {0} apenas pode conter letras e espaços em branco. Cada palavra começa em Maiúscula, seguida de minúsculas.")]
         public string Esquadra { get; set; }
 
         public string Fotografia { get; set; }
